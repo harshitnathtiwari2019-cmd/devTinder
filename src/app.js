938cd67e1dc548d3aa4d4cb6ee29d30c554179b4
Harshit Nath Tiwari <harshitnathtiwari2019@gmail.com>
2026-07-28 12:04:29 +0530

    playling with the routes

--- src/app.js
+++ src/app.js
@@ -2,31 +2,11 @@ const express = require("express");
 
 const app = express();
 
-app.use("/user", (req,res) => {
-  res.send("HAHAHAHAHAHAH")
-})
-
-
-
-// This will  only handle  GET call to /user
-app.get("/user", (req, res) => {
+app.get("/user/:userId/:name/:password", (req, res) => {
+  console.log(req.params);
   res.send({ firstName: "Harshit", lastName: "Tiwari" });
 });
 
-app.post("/user",(req, res) => {
-    // Saving data to db
-    res.send("Data successfully saved to the database");
-  });
-
-app.delete("/user",(req,res) => {
-  res.send("Deleted succuessfully");
-});  
-
-// This will match all the HTTP method API calls to  /test
-app.use("/test", (req, res) => {
-  res.send("Hello from the server!");
-});
-
-app.listen(7777, () => {
-  console.log("Server is successfully listening on port 7777...");
+app.listen(6666, () => {
+  console.log("Server is successfully listening on port 6666...");
 });

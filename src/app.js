237c1075d792b54d6ccc7665bb7a4ec333bdaadf
Harshit Nath Tiwari <harshitnathtiwari2019@gmail.com>
2026-07-28 09:11:45 +0530

    exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -2,14 +2,29 @@ const express = require("express");
 
 const app = express();
 
+app.use("/user", (req,res) => {
+  res.send("HAHAHAHAHAHAH")
+})
 
 
-app.use("/hello", (req, res) => {
-  res.send("Hello Hello!");
+
+// This will  only handle  GET call to /user
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Harshit", lastName: "Tiwari" });
 });
 
+app.post("/user",(req, res) => {
+    // Saving data to db
+    res.send("Data successfully saved to the database");
+  });
+
+app.delete("/user",(req,res) => {
+  res.send("Deleted succuessfully");
+});  
+
+// This will match all the HTTP method API calls to  /test
 app.use("/test", (req, res) => {
-  res.send("Hello Hello Hello!");
+  res.send("Hello from the server!");
 });
 
 app.listen(7777, () => {

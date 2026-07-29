5dd54f591c8c8894c6e17b568a700d7e1addf3b7
Harshit Nath Tiwari <harshitnathtiwari2019@gmail.com>
2026-07-29 13:02:43 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -2,10 +2,31 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user/:userId/:name/:password", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "Harshit", lastName: "Tiwari" });
-});
+app.use("/user", [
+  (req, res, next) => {
+    console.log("Handling the route user!!");
+    next();
+    // res.send("Response!!");
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 2!!");
+
+    // res.send("2nd Response!!");
+    next();
+  },
+],
+  (req, res, next) => {
+    console.log("Handling the route user 3!!");
+
+    //res.send("3rd Response!!");
+    next();
+  },
+  (req, res) => {
+    console.log("Handling the route user 4!!");
+
+    res.send("4th Response!!");
+  },
+);
 
 app.listen(6666, () => {
   console.log("Server is successfully listening on port 6666...");

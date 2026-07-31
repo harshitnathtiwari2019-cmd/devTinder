b211e3598e5ef5f78718be89c0a9ed92eb6e1a38
Harshit Nath Tiwari <harshitnathtiwari2019@gmail.com>
2026-07-31 20:02:02 +0530

    wildcard-error-handling

--- src/app.js
+++ src/app.js
@@ -2,26 +2,40 @@ const express = require("express");
 
 const app = express();
 
-const {adminAuth,userAuth} = require("./middlewares/auth");
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    // Log your error message
+    res.status(500).send("something went wrong");
+  }
 
-app.use("/admin", adminAuth);
-
-
-app.post("/user/login", (req,res) => {
-  res.send("User logged in successfully!")
 });
 
-app.get("/user/data", userAuth, (req, res) => {
-  res.send("User Data Sent");
+
+app.get("/getUserData", (req, res) => {
+  // try{
+
+  // Logical of DB call and user data
+  throw new Error("dkjkjjkkd");
+   res.send("User Data Sent");
+  }
+  // catch(err){
+  //  res.status(500).send("Some Error contact support team");
+  // }
+ 
+//  }
+);
+
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    // Log your error message
+    res.status(500).send("something went wrong");
+  }
+
 });
 
-app.get("/admin/getAllData", (req, res) => {
-  res.send("All Data Sent");
-});
 
-app.get("/admin/deleteUser", (req, res) => {
-  res.send("Delete a user Data Sent");
-});
+
+
 
 app.listen(6666, () => {
   console.log("Server is successfully listening on port 6666...");

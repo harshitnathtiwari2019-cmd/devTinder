b600027e7a172b9f7ac2d724814d0b39330fd460
Harshit Nath Tiwari <harshitnathtiwari2019@gmail.com>
2026-08-20 00:06:45 +0530

    created login api

--- src/app.js
+++ src/app.js
@@ -2,19 +2,63 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
+const { validateSignUpdata } = require("./utils/validation");
+const bcrypt = require("bcrypt");
 
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-  const user = new User(req.body);
-
   try {
+    // Validation of data
+
+    validateSignUpdata(req);
+
+    const {firstName, lastName, emailId,  password } = req.body;
+
+    // Encrypt the password
+
+    const passwordHash = await bcrypt.hash(password, 10);
+    console.log(passwordHash);
+
+    // Creating a new instance of the User model
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
+
     await user.save();
     res.send("User Added successfully!");
   } catch (err) {
-    res.status(400).send("Error saving th user:" + err.message);
+    res.status(400).send("ERROR:" + err.message);
   }
 });
+
+app.post("/login", async (req,res) => {
+  try{
+
+    const { emailId, password } = req.body;
+
+
+    const user = await User.findOne({emailId: emailId});
+    if(!user){
+      throw new Error("Invalid credentials");
+    }
+
+    const isPasswordValid = await bcrypt.compare(password,user.password);
+
+    if(isPasswordValid) {
+      res.send("Login Successful!!!");
+    }
+    else{
+      throw new Error("Invalid credentials");
+    }
+   
+  }catch (err) {
+    res.status(400).send("ERROR:" + err.message);
+  }
+})
 
 // Get user by email
 app.get("/user", async (req, res) => {
@@ -49,6 +93,8 @@ app.get("/feed", async (req, res) => {
   }
 });
 
+
+//  Delet a user from the database
 app.delete("/user", async (req, res) => {
   const userId = req.body.userId;
   try {
@@ -68,22 +114,17 @@ app.patch("/user/:userId", async (req, res) => {
 
   const data = req.body;
 
-  
-
-
   try {
-    const ALLOWED_UPDATES = [ 
-    "photoUrl", "about", "gender", "age", "skills"
-  ];
+    const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
 
-   const isUpdateAllowed = Object.keys(data).every((k) =>
-    ALLOWED_UPDATES.includes(k)
-
-   );
-   if(!isUpdateAllowed){
-    throw new Error ("Update not allowed ");
-   }
-    const user = await User.findByIdAndUpdate({ _id: userId }, data,{returnDocument: "after",
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k),
+    );
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed ");
+    }
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
       runValidators: true,
     });
     console.log(user);

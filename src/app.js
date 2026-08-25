86b0ff1eec065b4377715fa59d97d44a50a3c282
Harshit Nath Tiwari <harshitnathtiwari2019@gmail.com>
2026-08-25 19:52:00 +0530

    Refactoring the code using mongoose schema methods

--- src/app.js
+++ src/app.js
@@ -48,14 +48,12 @@ app.post("/login", async (req, res) => {
       throw new Error("Invalid credentials");
     }
 
-    const isPasswordValid = await bcrypt.compare(password, user.password);
+    const isPasswordValid = await user.validatePassword(password);
 
     if (isPasswordValid) {
       //  Create a JWT Token
 
-      const token = await jwt.sign({ _id: user._id }, "DEV@Tinder$790", {
-        expiresIn: "7d",
-      });
+      const token = await user.getJWT();
 
       //  Add the  token to cookie  and send the response back to the user
       res.cookie("token", token, {
